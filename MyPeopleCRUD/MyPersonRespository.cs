79d0543db2776bcaf24215141f86d2be92b419c0
Mariusz Bardyn <mariusz.bardyn@gmail.com>
2018-09-01 14:30:24 +0200

    Add files via upload

---
+++ MyPeopleCRUD/MyPersonRespository.cs
@@ -0,0 +1,74 @@
+﻿using LiteDB;
+using MyMovieYDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPeopleCRUD
+{
+    public class MyPersonRespository
+    {
+        private string _connectionPath = @"C:\Users\Mariusz\Desktop\Person";
+
+        public Person GetById(int id)
+        {
+            //zamayka bazę danych
+            using (var db = new LiteDatabase(_connectionPath))
+            {
+                //odnośnik do repozytorium, kolekcja
+                var repository = db.GetCollection<Person>();
+
+                return repository.FindById(id);
+            }
+        }
+
+        public List<Person> GetAll()
+        {
+            using (var db = new LiteDatabase(_connectionPath))
+            {
+                //odnośnik do repozytorium, , kolekcja
+                var repository = db.GetCollection<Person>();
+
+                return repository.FindAll().ToList();
+            }
+        }
+
+        public int Create(Person Person)
+        {
+            using (var db = new LiteDatabase(_connectionPath))
+            {
+                //odnośnik do repozytorium, , kolekcja
+                var repository = db.GetCollection<Person>();
+                repository.Insert(Person);
+
+                return Person.Id;
+            }
+        }
+
+        public void Update(Person Person)
+        {
+            using (var db = new LiteDatabase(_connectionPath))
+            {
+                //odnośnik do repozytorium, , kolekcja
+                var repository = db.GetCollection<Person>();
+                repository.Update(Person);
+
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var db = new LiteDatabase(_connectionPath))
+            {
+                //odnośnik do repozytorium, , kolekcja
+                var repository = db.GetCollection<Person>();
+                repository.Delete(id);
+
+
+            }
+        }
+    }
+}
+
---
+++ MyPeopleCRUD/MyReviewRespository.cs
@@ -0,0 +1,93 @@
+﻿using LiteDB;
+using MyMovieYDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPeopleCRUD
+{
+    public class MyReviewRespository
+    {
+        private string _connectionPath = @"C:\Users\Mariusz\Desktop\Review";
+
+        public Review GetById(int id)
+        {
+
+
+            //zamayka bazę danych
+            using (var db = new LiteDatabase(_connectionPath))
+            {
+                //odnośnik do repozytorium, kolekcja
+                var repository = db.GetCollection<Review>();
+
+                return repository.FindById(id);
+            }
+        }
+
+        public List<Review> GetAll()
+        {
+            using (var db = new LiteDatabase(_connectionPath))
+            {
+                //odnośnik do repozytorium, , kolekcja
+                var repository = db.GetCollection<Review>();
+
+                return repository.FindAll().ToList();
+            }
+        }
+
+        public List<Review> GetByAuthor(int authorId)
+        {
+            using (var db = new LiteDatabase(_connectionPath))
+            {
+                var repository = db.GetCollection<Review>();
+                return repository.Find(x => x.Author.Id == authorId).ToList();
+            }
+        }
+
+        public List<Review> GetByMovie(int movieId)
+        {
+            using (var db = new LiteDatabase(_connectionPath))
+            {
+                var repository = db.GetCollection<Review>();
+                return repository.Find(x => x.MovieId == movieId).ToList();
+            }
+        }
+
+        public int Create(Review Review)
+        {
+            using (var db = new LiteDatabase(_connectionPath))
+            {
+                //odnośnik do repozytorium, , kolekcja
+                var repository = db.GetCollection<Review>();
+                repository.Insert(Review);
+
+                return Review.Id;
+            }
+        }
+
+        public void Update(Review Review)
+        {
+            using (var db = new LiteDatabase(_connectionPath))
+            {
+                //odnośnik do repozytorium, , kolekcja
+                var repository = db.GetCollection<Review>();
+                repository.Update(Review);
+
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var db = new LiteDatabase(_connectionPath))
+            {
+                //odnośnik do repozytorium, , kolekcja
+                var repository = db.GetCollection<Review>();
+                repository.Delete(id);
+
+
+            }
+        }
+    }
+}
